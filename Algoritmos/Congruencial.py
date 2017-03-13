f6e03d5ebe73f5b63f3341bebe5f73fc773988e9
jacobotapia <jacob.et.cetera@gmail.com>
2017-03-13 16:14:27 -0600

    init3

--- Algoritmos/Congruencial.py
+++ Algoritmos/Congruencial.py
@@ -1,3 +1,15 @@
 import numpy as np
 
+def congruencial(x,a,c,m,iter = 10):
+
+    generador = []
+    operacion = []
+    aleatorio = []
+    ri = []
+
+    for x in iter:
+        pass
+
+
+    return generador,operacion,aleatorio,ri
 
