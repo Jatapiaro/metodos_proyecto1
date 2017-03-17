54e59d80c54aab986cdf17aa5361be9bbd69dc41
Jacobo <jacob.et.cetera@gmail.com>
2017-03-16 21:40:06 -0600

    Termina congruencial mixto

--- Algoritmos/CongruencialMixto.py
+++ Algoritmos/CongruencialMixto.py
@@ -5,7 +5,7 @@ xo = (aXo+c) mod m
 """
 xo = lambda a,x,c,m: ((a*x)+c)%m
 
-def congruencial(x,a,c,m,iter=50):
+def congruencial_mixto(x,a,c,m,iter=50):
     xn = []
     xni = []
     random = []
@@ -15,12 +15,16 @@ def congruencial(x,a,c,m,iter=50):
             ni = xo(a,x,c,m)
             xni.append(ni)
             random.append(ni/m)
+            x = ni
     else:
         for i in range(0,iter):
             xn.append(x)
             ni = xo(a,x,c,m)
             xni.append(ni)
             random.append(ni/m)
+            x = ni
+            if ni in xn:
+                break
 
     return xn,xni,random
 
